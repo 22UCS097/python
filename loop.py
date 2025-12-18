9ca2ce06d6e6cf789a4ba0d48956677cea2fa9c1
22UCS097 <ravikumar628763@gmail.com>
2025-12-18 22:02:46 +0530

    more loops

--- loop.py
+++ loop.py
@@ -18,12 +18,94 @@
 # print(sum)
 
 
-n=5
-for it in range(11):
-    if it==5 or it==0:
-        continue
-    else:
-        print(it*n)
+# n=5
+# for it in range(11):
+#     if it==5 or it==0:
+#         continue
+#     else:
+#         print(it*n)
+
+# reverse string:
+
+# str="ravikumar"
+# revStr=""
+
+# for char in str:
+#     revStr=char+revStr
+# print(revStr)    
+
+# find non repeating char
+
+# str="taeetr"
+
+# for char in str:
+#     if str.count(char)==1:
+#         print(char)
+#         break
+
+# 5!
+
+# num=5
+# fact=1
+
+# while num>0:
+#     fact=fact*num
+#     num-=1
+
+# print(fact)    
+
+
+# while 1:
+#     num=int(input("enter value b/w 1 and 10\n"))
+#     if num>10 or num<0:
+#         break
+
+#  prime no.
+
+# num=int(input("enter no."))
+# is_prime=1
+
+# if num>1:
+#     for i in range(2,num):
+#         if (num%i)==0:
+#             is_prime=0
+#             break
+
+
+# print(is_prime)       
+# 
+
+# items=["apple","grapes","mango","apple"]
+
+# unique_item=set()
+
+# for item in items:
+#     if item in unique_item:
+#         print("YES")
+#         break
+#     unique_item.add(item)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
